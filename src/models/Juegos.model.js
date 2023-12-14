5695850ebca42750872640d94280adc0c89ccffc
brahian github <brahiangithub@gmail.com>
2023-12-14 15:40:53 -0500

    entendiendo validaciones

--- src/models/Juegos.model.js
+++ src/models/Juegos.model.js
@@ -2,10 +2,23 @@
 const { Schema, model, SchemaType} = require("mongoose") //Utilizar dos caracteristicas de mongo
 
 const JuegoSchema = new Schema({ 
-   nombre: { type: String },
-   descripcion: { type: String },
-   genero_id: { type: Schema.Types.ObjectId, ref: "generos" }, //esquema,tipo,obid mas referencia del modelo a agregar
-   fechaLanzamiento: { type: String },
+   nombre: { 
+      type: String,
+      required: [true,'Nombre requerido']
+   },
+   descripcion: { 
+      type: String,
+      required: [true,'Descripcion requerida']
+   },
+   genero_id: { 
+      type: Schema.Types.ObjectId, 
+      ref: "generos", //esquema,tipo,obid mas referencia del modelo a agregar
+      required: [true,'Falta genero del videojuego']
+   }, 
+   fechaLanzamiento: { 
+      type: String,
+      required: [true,'Agregar fecha de lanzamiento']
+   },
 })
 
 //Exportar el esquema en el modelo
